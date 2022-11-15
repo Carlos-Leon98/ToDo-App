fcee6ab5296b319365e455b6de339ec0bbdada3e
Carlos Leon <carlosaleonrivas@gmail.com>
2022-11-14 22:40:57 -0600

    Adding some html and css

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 const path = require('path');
-const fr = require('fs');
+const fs = require('fs');
 const { connectDB } = require('./config/db');
 const { router } = require('./routes/todo');
 
@@ -12,17 +11,19 @@ connectDB();
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(express.static(__dirname))
-app.use(express.json());
 
-if (process.env.MODE === 'development') {
-    app.use(morgan('dev'));
-}
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
 
 app.use('/api/task', router);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    fs.readFile(path.join(__dirname, "public", "index.html"), (error, data) => {
+        res.writeHead(200, ({ "Content-Type": "text/html" }));
+        res.write(data);
+        res.end()
+    })
 });
 
 app.listen(PORT, console.log(`Server is running in port: ${PORT}`));
